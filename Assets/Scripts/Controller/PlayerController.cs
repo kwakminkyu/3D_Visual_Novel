6784393d2bf8d51492bab83e87cb5e822c302887
KwakMinkyu <kmk3296@gmail.com>
2024-04-15 18:30:04 +0900

    [add] view rotation
    카메라 회전 구현

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -7,9 +7,35 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] Transform tf_Crosshair;
 
+    [SerializeField] Transform tf_Cam;
+
+    [SerializeField] float sightSensitivity;
+    [SerializeField] float lookLimitX;
+    [SerializeField] float lookLimitY;
+    float currentAngleX;
+    float currentAngleY;
+
     private void Update()
     {
         CrosshairMoving();
+        ViewMoving();
+    }
+
+    private void ViewMoving()
+    {
+        if (tf_Crosshair.localPosition.x > (Screen.width / 2) - 100 || tf_Crosshair.localPosition.x < (-Screen.width / 2) + 100)
+        {
+            currentAngleY += (tf_Crosshair.localPosition.x > 0) ? sightSensitivity : -sightSensitivity;
+            currentAngleY = Mathf.Clamp(currentAngleY, -lookLimitX, lookLimitX);
+        }
+
+        if (tf_Crosshair.localPosition.y > (Screen.height / 2) - 100 || tf_Crosshair.localPosition.y < (-Screen.height / 2) + 100)
+        {
+            currentAngleX += (tf_Crosshair.localPosition.y > 0) ? -sightSensitivity : sightSensitivity;
+            currentAngleX = Mathf.Clamp(currentAngleX, -lookLimitY, lookLimitY);
+        }
+
+        tf_Cam.localEulerAngles = new Vector3(currentAngleX, currentAngleY, tf_Cam.localEulerAngles.z);
     }
 
     private void CrosshairMoving()
